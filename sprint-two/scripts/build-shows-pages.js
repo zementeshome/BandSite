305f13df5ae3bfd7fc2a0494924e9f91013bc646
Zemen Teshome <zementeshome@gmail.com>
2020-09-03 19:42:19 -0400

    dom manipulation shows section styling for mobile

---
+++ sprint-two/scripts/build-shows-pages.js
@@ -0,0 +1,367 @@
+
+// shows section
+
+let main = document.createElement('main');
+  main.classList.add('main');
+
+let section = document.createElement('section');
+   section.classList.add('shows');
+
+ let showsInfo = document.createElement('div');
+ showsInfo.classList.add('shows__info');
+
+ let showsHeader = document.createElement('h2');
+ showsHeader.classList.add('shows__header');
+ console.log(showsHeader);
+ showsHeader.innerText = 'Shows';
+ showsInfo.appendChild(showsHeader);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+
+let showsDate = document.createElement('h3');
+ showsDate.classList.add('shows__date');
+ console.log(showsDate);
+ showsDate.innerText = 'DATE';
+ showsInfo.appendChild(showsDate);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDateDetails = document.createElement('p');
+showsDateDetails.classList.add('shows__date-details');
+showsDateDetails.innerText = 'Mon Dec 17 2018';
+showsInfo.appendChild(showsDateDetails);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue = document.createElement('h3');
+showsVenue.classList.add('shows__venue');
+showsVenue.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails = document.createElement('p')
+showsVenueDetails.classList.add('shows__venue-details');
+showsVenueDetails.innerText = 'Ronald Lane';
+showsInfo.appendChild(showsVenueDetails);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation = document.createElement('h3');
+showsLocation.classList.add('shows__location');
+showsLocation.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails = document.createElement('p');
+showsLocationDetails.classList.add('shows__location-details');
+showsLocationDetails.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton = document.createElement('button');
+showsButton.classList.add('shows__button');
+showsButton.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline = document.createElement('span');
+showsButtonUnderline.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDate2 = document.createElement('h3');
+ showsDate2.classList.add('shows__date');
+ showsDate2.innerText = 'DATE';
+ showsInfo.appendChild(showsDate2);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+ let showsDateDetails2 = document.createElement('p');
+ showsDateDetails2.classList.add('shows__date-details');
+ showsDateDetails2.innerText = 'Tue Jul 18 2019';
+ showsInfo.appendChild(showsDateDetails2);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue2 = document.createElement('h3');
+showsVenue2.classList.add('shows__venue');
+showsVenue2.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails2 = document.createElement('p')
+showsVenueDetails2.classList.add('shows__venue-details');
+showsVenueDetails2.innerText = 'Pier 3 East';
+showsInfo.appendChild(showsVenueDetails2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation2 = document.createElement('h3');
+showsLocation2.classList.add('shows__location');
+showsLocation2.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails2 = document.createElement('p');
+showsLocationDetails2.classList.add('shows__location-details');
+showsLocationDetails2.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton2 = document.createElement('button');
+showsButton2.classList.add('shows__button');
+showsButton2.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline2 = document.createElement('span');
+showsButtonUnderline2.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline2);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDate3 = document.createElement('h3');
+ showsDate3.classList.add('shows__date');
+ showsDate3.innerText = 'DATE';
+ showsInfo.appendChild(showsDate3);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+ let showsDateDetails3 = document.createElement('p');
+ showsDateDetails3.classList.add('shows__date-details');
+ showsDateDetails3.innerText = 'Fri July 22 2019';
+ showsInfo.appendChild(showsDateDetails3);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue3 = document.createElement('h3');
+showsVenue3.classList.add('shows__venue');
+showsVenue3.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails3 = document.createElement('p')
+showsVenueDetails3.classList.add('shows__venue-details');
+showsVenueDetails3.innerText = 'View Loungue';
+showsInfo.appendChild(showsVenueDetails3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation3 = document.createElement('h3');
+showsLocation3.classList.add('shows__location');
+showsLocation3.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails3 = document.createElement('p');
+showsLocationDetails3.classList.add('shows__location-details');
+showsLocationDetails3.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton3 = document.createElement('button');
+showsButton3.classList.add('shows__button');
+showsButton3.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline3 = document.createElement('span');
+showsButtonUnderline3.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline3);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDate4 = document.createElement('h3');
+ showsDate4.classList.add('shows__date');
+ showsDate4.innerText = 'DATE';
+ showsInfo.appendChild(showsDate4);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+ let showsDateDetails4 = document.createElement('p');
+ showsDateDetails4.classList.add('shows__date-details');
+ showsDateDetails4.innerText = 'Sat Aug 12 2019';
+ showsInfo.appendChild(showsDateDetails4);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue4 = document.createElement('h3');
+showsVenue4.classList.add('shows__venue');
+showsVenue4.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails4 = document.createElement('p')
+showsVenueDetails4.classList.add('shows__venue-details');
+showsVenueDetails4.innerText = 'Hyatt Agency';
+showsInfo.appendChild(showsVenueDetails4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation4 = document.createElement('h3');
+showsLocation4.classList.add('shows__location');
+showsLocation4.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails4 = document.createElement('p');
+showsLocationDetails4.classList.add('shows__location-details');
+showsLocationDetails4.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton4 = document.createElement('button');
+showsButton4.classList.add('shows__button');
+showsButton4.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline4 = document.createElement('span');
+showsButtonUnderline4.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline4);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDate5 = document.createElement('h3');
+ showsDate5.classList.add('shows__date');
+ showsDate5.innerText = 'DATE';
+ showsInfo.appendChild(showsDate5);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+ let showsDateDetails5 = document.createElement('p');
+ showsDateDetails5.classList.add('shows__date-details');
+ showsDateDetails5.innerText = 'Fri Sep 05 2019';
+ showsInfo.appendChild(showsDateDetails5);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue5 = document.createElement('h3');
+showsVenue5.classList.add('shows__venue');
+showsVenue5.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails5 = document.createElement('p')
+showsVenueDetails5.classList.add('shows__venue-details');
+showsVenueDetails5.innerText = 'Moscow Center';
+showsInfo.appendChild(showsVenueDetails5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation5 = document.createElement('h3');
+showsLocation5.classList.add('shows__location');
+showsLocation5.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails5 = document.createElement('p');
+showsLocationDetails5.classList.add('shows__location-details');
+showsLocationDetails5.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton5 = document.createElement('button');
+showsButton5.classList.add('shows__button');
+showsButton5.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline5 = document.createElement('span');
+showsButtonUnderline5.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline5);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsDate6 = document.createElement('h3');
+ showsDate6.classList.add('shows__date');
+ showsDate6.innerText = 'DATE';
+ showsInfo.appendChild(showsDate6);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+ let showsDateDetails6 = document.createElement('p');
+ showsDateDetails6.classList.add('shows__date-details');
+ showsDateDetails6.innerText = 'Wed Aug 11 2019';
+ showsInfo.appendChild(showsDateDetails6);
+ document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenue6 = document.createElement('h3');
+showsVenue6.classList.add('shows__venue');
+showsVenue6.innerText = 'VENUE';
+showsInfo.appendChild(showsVenue6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsVenueDetails6 = document.createElement('p')
+showsVenueDetails6.classList.add('shows__venue-details');
+showsVenueDetails6.innerText = 'Pres Club';
+showsInfo.appendChild(showsVenueDetails6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocation6 = document.createElement('h3');
+showsLocation6.classList.add('shows__location');
+showsLocation6.innerText = 'LOCATION';
+showsInfo.appendChild(showsLocation6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsLocationDetails6 = document.createElement('p');
+showsLocationDetails6.classList.add('shows__location-details');
+showsLocationDetails6.innerText = 'San Francisco, CA';
+showsInfo.appendChild(showsLocationDetails6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButton6 = document.createElement('button');
+showsButton6.classList.add('shows__button');
+showsButton6.innerText = 'BUY TICKETS';
+showsInfo.appendChild(showsButton6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+let showsButtonUnderline6 = document.createElement('span');
+showsButtonUnderline6.classList.add('shows__button-underline');
+showsInfo.appendChild(showsButtonUnderline6);
+document.body.querySelector('div').appendChild(showsInfo);
+
+
+/* <section class="shows">
+    <h2 class="shows__header">Shows</h2>
+      <div class="shows__info">
+          <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Mon Dec 17 2018</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">Ronald Lane</p>
+              <h3 class="shows__location">LOCATION</h3>
+          <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+          <div class="shows__button-underline"></div>
+      <div class="shows__info">
+              <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Tue Jul 18 2019</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">Pier 3 East</p>
+              <h3 class="shows__location">LOCATION</h3>
+              <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+      <div class="shows__button-underline"></div>
+      <div class="shows__info">
+              <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Fri Jul 22 2019</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">View Loungue</p>
+              <h3 class="shows__location">LOCATION</h3>
+              <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+      <div class="shows__button-underline"></div>
+      <div class="shows__info">
+              <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Sat Aug 21 2019</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">Hyatt Agency</p>
+              <h3 class="shows__location">LOCATION</h3>
+              <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+      <div class="shows__button-underline"></div>
+      <div class="shows__info">
+              <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Fri Sep 05 2019</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">Moscow Center</p>
+              <h3 class="shows__location">LOCATION</h3>
+              <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+      <div class="shows__button-underline"></div>
+      <div class="shows__info">
+              <h3 class="shows__date">DATE</h3>
+              <p class="shows__date-details">Wed Aug 11 2019</p>
+              <h3 class="shows__venue">VENUE</h3>
+              <p class="shows__venue-details">Pres Club</p>
+              <h3 class="shows__location">LOCATION</h3>
+              <p class="shows__location-details">San Francisco, CA</p>
+      </div>
+          <button class="shows__button">BUY TICKETS</button>
+      <div class="shows__button-underline"></div>
+  </section> */
